404ae10ab8c08639adc55f4a795cd5c549a4a827
Daniel Setréus <daniel.setreus@gmail.com>
2022-03-11 12:07:06 +0100

    Fix spacing in deprecation comment markdown

--- src/Attributes/RouteDeprecated.php
+++ src/Attributes/RouteDeprecated.php
@@ -14,6 +14,6 @@ class RouteDeprecated extends RouteAttribute
 
     public function toMarkdown(): string
     {
-        return '**Deprecated!** ' . $this->comment;
+        return '**Deprecated!**' . (strlen($this->comment > 0) ? ' ' . $this->comment : '');
     }
 }
