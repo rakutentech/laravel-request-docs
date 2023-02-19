1d3d641fbcec5e3c207dc423491637a335253915
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-02-19 14:15:18 +0900

    (v2.1) lint

--- src/LaravelRequestDocs.php
+++ src/LaravelRequestDocs.php
@@ -357,10 +357,10 @@ class LaravelRequestDocs
         return $params;
     }
 
-    private function multiexplode($delimiters, $string) {
-
+    private function multiexplode($delimiters, $string)
+    {
         $ready = str_replace($delimiters, $delimiters[0], $string);
         $launch = explode($delimiters[0], $ready);
         return  $launch;
-    }    
+    }
 }
