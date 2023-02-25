c156f9bf23e4a0e9f5bdcb0112df2430a631c6ff
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-02-25 20:37:26 +0900

    (v2.5) Clear localstorage button in settings

--- ui/src/components/TopNav.tsx
+++ ui/src/components/TopNav.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import useLocalStorage from 'react-use-localstorage';
-import { MagnifyingGlassIcon, Cog6ToothIcon, ArrowUpRightIcon, MoonIcon, SunIcon, XMarkIcon, ChatBubbleLeftIcon  } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon, Cog6ToothIcon, ArrowUpRightIcon, MoonIcon, SunIcon, XMarkIcon, ChatBubbleLeftIcon } from '@heroicons/react/24/solid'
 
 interface Props {
     handleChangeSettings: (
@@ -62,6 +62,11 @@ export default function TopNav(props: Props) {
         handleChangeSettings(showGet, showPost, showDelete, showPut, showPatch, e.target.checked, sort, groupby)
     }
 
+    const handleClearLocalStorage = () => {
+        localStorage.clear()
+        window.location.reload()
+    }
+
 
     const toggleDarkMode = () => {
         const dataTheme = document.documentElement.getAttribute('data-theme');
@@ -108,22 +113,22 @@ export default function TopNav(props: Props) {
                 </div>
                 <div className="flex-none">
                     <div className="form-control">
-                    <label htmlFor="search" className="relative text-gray-400 focus-within:text-gray-600 block">
-                        <MagnifyingGlassIcon className="pointer-events-none w-4 h-4 absolute top-1/2 transform -translate-y-1/2 left-3" />
-                        <input type="text" placeholder="Filter APIs" className="input pl-10 input-sm input-bordered" onChange={ (e) => handleSearch(e.target.value) } />
-                    </label>                        
-                        
-                    </div>                    
+                        <label htmlFor="search" className="relative text-gray-400 focus-within:text-gray-600 block">
+                            <MagnifyingGlassIcon className="pointer-events-none w-4 h-4 absolute top-1/2 transform -translate-y-1/2 left-3" />
+                            <input type="text" placeholder="Filter APIs" className="input pl-10 input-sm input-bordered" onChange={(e) => handleSearch(e.target.value)} />
+                        </label>
+
+                    </div>
                     <div className="menu menu-horizontal px-6 ">
                         <label className="swap swap-rotate">
                             <input type="checkbox" onChange={toggleDarkMode} />
-                            {theme === 'dark' ? <SunIcon className="h-6 w-6"/> : <MoonIcon className="h-6 w-6"/>}
+                            {theme === 'dark' ? <SunIcon className="h-6 w-6" /> : <MoonIcon className="h-6 w-6" />}
                         </label>
                     </div>
                     <div className="ml-1">
                         <a href="#modal-settings" className="btn btn-ghost btn-sm">
                             <span className="pr-1">
-                                <Cog6ToothIcon className="h-6 w-6"/>
+                                <Cog6ToothIcon className="h-6 w-6" />
                             </span>
                         </a>
                         <div className="modal" id="modal-settings">
@@ -132,13 +137,13 @@ export default function TopNav(props: Props) {
                                 <h4 className="font-bold mt-10">Sort By</h4>
                                 <div className="form-control">
                                     <label className="label">
-                                        
+
                                         <input type="radio" onChange={handleChangeSort} value="default" className="radio" checked={sort == "default"} />
                                         <span className="label-text">Default</span>
-                                        
+
                                         <input type="radio" onChange={handleChangeSort} value="route_names" className="radio" checked={sort == "route_names"} />
-                                        <span className="label-text">Routes</span>
-                                        
+                                        <span className="label-text">Route Names</span>
+
                                         <input type="radio" onChange={handleChangeSort} value="method_names" className="radio" checked={sort == "method_names"} />
                                         <span className="label-text">HTTP Methods</span>
                                     </label>
@@ -146,15 +151,15 @@ export default function TopNav(props: Props) {
                                 <h4 className="font-bold mt-10">Group By</h4>
                                 <div className="form-control">
                                     <label className="label">
-                                        
+
                                         <input type="radio" onChange={handleChangeGroupby} value="default" className="radio" checked={groupby == "default"} />
                                         <span className="label-text">Default</span>
-                                        
+
                                         <input type="radio" onChange={handleChangeGroupby} value="api_uri" className="radio" checked={groupby == "api_uri"} />
-                                        <span className="label-text">API URI</span>
-                                        
+                                        <span className="label-text">API Name</span>
+
                                         <input type="radio" onChange={handleChangeGroupby} value="controller_full_path" className="radio" checked={groupby == "controller_full_path"} />
-                                        <span className="label-text">Controller Names</span>
+                                        <span className="label-text">Controller Name</span>
                                     </label>
                                 </div>
                                 <h4 className="font-bold mt-10">Display Settings</h4>
@@ -184,9 +189,19 @@ export default function TopNav(props: Props) {
                                         <input type="checkbox" onChange={handleChangeHead} className="toggle toggle-success" checked={showHead == 'true'} />
                                     </label>
                                 </div>
+                                <h4 className="font-bold mt-10">Storage</h4>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">
+                                            Clear localstorage
+                                            <p><small>Delete localstorage data, request body and queries</small></p>
+                                        </span>
+                                        <button className="btn btn-sm btn-error" onClick={handleClearLocalStorage}>Clear</button>
+                                    </label>
+                                </div>
                                 <div className="modal-action">
                                     <a href="#" className="btn btn-sm">
-                                        <XMarkIcon className="h-6 w-6"/> Close
+                                        <XMarkIcon className="h-6 w-6" /> Close
                                     </a>
                                 </div>
                             </div>
@@ -195,16 +210,16 @@ export default function TopNav(props: Props) {
                     <div className="ml-1 ">
                         <a className="btn btn-ghost btn-sm" href='/request-docs/api?openapi=true' target="_blank">
                             <span className="pr-1">
-                                <ArrowUpRightIcon className="h-6 w-6"/>
-                            </span>                             
+                                <ArrowUpRightIcon className="h-6 w-6" />
+                            </span>
                             OpenAPI 3.0
                         </a>
                     </div>
                     <div className="ml-1 ">
                         <a className="btn btn-ghost btn-sm" href='https://github.com/rakutentech/laravel-request-docs/issues/new' target="_blank" rel="noreferrer">
                             <span className="pr-1">
-                            <ChatBubbleLeftIcon className="h-6 w-6"/>
-                            </span>                             
+                                <ChatBubbleLeftIcon className="h-6 w-6" />
+                            </span>
                             Feature request
                         </a>
                     </div>
