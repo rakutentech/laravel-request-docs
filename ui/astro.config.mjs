4a78ec9bda5e7844e4c1ef2961d7c527e2da1803
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-08-07 09:33:57 +0900

    oops

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,20 +1,17 @@
-import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import compress from '@otterlord/astro-compress'
+import react from '@astrojs/react'
 
-import git from 'git-rev-sync';
-const version = git.tag();
-const versionParts = version.split('.');
-const versionIncremented = versionParts[0] + '.' + (parseInt(versionParts[1]) + 1);
-console.log('Version: ' + version + ' -> ' + versionIncremented);
-process.env.PUBLIC_VERSION = versionIncremented;
+import git from 'git-rev-sync'
+const version = git.tag()
+const versionParts = version.split('.')
+const versionIncremented =
+    versionParts[0] + '.' + (parseInt(versionParts[1]) + 1)
+console.log('Version: ' + version + ' -> ' + versionIncremented)
+process.env.PUBLIC_VERSION = versionIncremented
 
 export default defineConfig({
-
-  integrations: [
-    compress(),
-    react()
-  ],
-  outDir: '../resources/dist',
-  base: process.env.PUBLIC_BASE || '/request-docs',
-});
+    integrations: [compress(), react()],
+    outDir: '../resources/dist',
+    base: process.env.PUBLIC_BASE || '/request-docs'
+})
